cd449b0c801eeaedbf29862d5f81a84978c0f769
PtakhovD <ptakhov@gmail.com>
2021-02-02 00:43:09 +1000

    Рабочий код

---
+++ py_parsing/items.py
@@ -0,0 +1,24 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class PyParsingItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    pass
+
+
+class AutoyoulaItem(scrapy.Item):
+    _id = scrapy.Field()
+    url = scrapy.Field()
+    title = scrapy.Field()
+    images = scrapy.Field()
+    description = scrapy.Field()
+    author = scrapy.Field()
+    specifications = scrapy.Field()
+    price = scrapy.Field()
+    phone = scrapy.Field()
